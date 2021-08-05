aa02cb0fd4c3a5e84fab4e494b71e4f21803b831
Juan Junger <juan.junger@descomplica.com.br>
2021-08-05 15:52:34 -0300

    update register page

--- pages/Register/index.tsx
+++ pages/Register/index.tsx
@@ -1,20 +1,49 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text, TextInput, Button } from 'react-native-paper';
 
 const Register = () => {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+
+  const handleAccountCreation = async () => {
+    alert("Pressed")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Register</Text>  
+      <Text>Cadastre-se</Text>
+
+      <TextInput
+        label="Nome"
+        value={name}
+        onChangeText={name => setName(name)}
+      />
+
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={email => setEmail(email)}
+      />
+
+      <TextInput
+        label="Password"
+        secureTextEntry
+        right={<TextInput.Icon name="eye" />}
+      />
+
+      <Button mode="contained" onPress={handleAccountCreation}>
+        Criar conta
+      </Button>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: "100%",
     backgroundColor: "#f4f4f4",
-    alignItems: "center",
-    justifyContent: "center",
   }
 });
 
